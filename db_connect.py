f3cd02b61834f3f4e639a58a3b863651329acc5d
Jason <jp3477@columbia.edu>
2017-04-27 19:16:57 -0400

    Adding database system for keeping track of traces

---
+++ db_connect.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine
+engine = create_engine('postgresql://jason:password@localhost/trace_db', echo=True)
+
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Session(Base):
+    __tablename__ = 'session'
+
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime)
+    plot_filepath = Column(String)
+    mouse_id = Column(Integer, ForeignKey('mouse.id'))
+    mouse = relationship('Mouse', back_populates='sessions')
+
+class Mouse(Base):
+    __tablename__ = 'mouse'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    sessions = relationship('Session', back_populates='mouse')
+
+
+
+# Base.metadata.create_all(engine)
